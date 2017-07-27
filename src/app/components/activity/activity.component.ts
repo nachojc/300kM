74ddbc5a45562626e9bc10cc001e81546fee8d6f
Ignacio Jimenez <nachojc@ipsoft.es>
2017-07-27 20:59:40 +0200

    pequeña actualizacion

--- src/app/components/activity/activity.component.ts
+++ src/app/components/activity/activity.component.ts
@@ -5,4 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './activity.component.html',
   styleUrls: ['./activity.component.scss']
 })
-export class ActivityComponent { }
+export class ActivityComponent {
+
+
+  public act={avatar:"https://lh3.googleusercontent.com/-QiCYk5LjgZk/AAAAAAAAAAI/AAAAAAAAADU/4KpOrrYEz9o/s120-p-rw-no/photo.jpg",
+             images:['https://lh3.googleusercontent.com/-QiCYk5LjgZk/AAAAAAAAAAI/AAAAAAAAADU/4KpOrrYEz9o/s120-p-rw-no/photo.jpg','https://lh3.googleusercontent.com/-QiCYk5LjgZk/AAAAAAAAAAI/AAAAAAAAADU/4KpOrrYEz9o/s120-p-rw-no/photo.jpg','https://lh3.googleusercontent.com/-QiCYk5LjgZk/AAAAAAAAAAI/AAAAAAAAADU/4KpOrrYEz9o/s120-p-rw-no/photo.jpg','https://lh3.googleusercontent.com/-QiCYk5LjgZk/AAAAAAAAAAI/AAAAAAAAADU/4KpOrrYEz9o/s120-p-rw-no/photo.jpg','https://lh3.googleusercontent.com/-QiCYk5LjgZk/AAAAAAAAAAI/AAAAAAAAADU/4KpOrrYEz9o/s120-p-rw-no/photo.jpg'],
+              name:"Diana Santos",
+              description:"ha creado un evento",
+              time:"27/07/2017 17:00"
+            }
+
+
+ }
--- src/app/ui/ui-cards/card-activity/card-activity.component.ts
+++ src/app/ui/ui-cards/card-activity/card-activity.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'card-activity',
   templateUrl: './card-activity.component.html',
   styleUrls: ['./card-activity.component.scss']
 })
-export class CardActivityComponent { 
-  public nameTitle:string="Diana Santos";
-  public textTitle:string="ha creado un evento";
-  public timeSlot:string="25 min ago";
-  public textnumber:string="+3";
+export class CardActivityComponent implements OnInit {
+  @Input() avatar: string;
+  @Input() images: string[];
 
+  @Input() name: string;
+  @Input() description: string;
+  @Input() time: string;
+
+  @Input() obj: any;
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.obj = this.obj ||
+      {
+        avatar: this.avatar,
+        images: this.images || [],
+        name: this.name,
+        description: this.description,
+        time: this.time
+      };
+    this.obj.number = this.obj.images && this.obj.images.length ? '+ ' + Math.max(this.obj.images.length - 2, 0) : 0;
+  }
 }
